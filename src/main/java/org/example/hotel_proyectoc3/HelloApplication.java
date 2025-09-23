a6a9168f08ec27057baf6befb3f9dd72172c5f03
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2025-09-22 18:14:56 -0600

    changes new

--- src/main/java/org/example/hotel_proyectoc3/HelloApplication.java
+++ src/main/java/org/example/hotel_proyectoc3/HelloApplication.java
@@ -15,6 +15,9 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
+        //Probando si es el repo correcto y actualizado.
     }
 
     public static void main(String[] args) {
